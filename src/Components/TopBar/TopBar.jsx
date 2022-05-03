89dad168a6856f4686218e9043d2f6eb3e039b2e
Seth <Medeirosseth@gamil.com>
2022-05-02 19:47:15 -0700

    nav bar img layout test

--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -4,8 +4,20 @@ import './topBar.css'
 export default function TopBar() {
   return (
     <div className='topBar'>
-      <img className="topBarImg" src='assets/JasonWMic.png' alt="Jason with Mic"/>
-    
+      <div className="imgContainer"> 
+        <img className="topBarImg" src='assets/JasonWMic.png' alt="Jason with Mic"/>
+      </div>
+      <div className='navLink'>
+        <div className="alltta">
+          <img className="allttaImg" src='assets/ALLTTA.jpg' alt="ALLTTA logo"/>
+        </div>
+        <div className="procussions">
+          <img className="procussionsImg" src='assets/Procussions.jpg' alt="Procussions Logo"/>
+        </div>
+        <div className="knives">
+          <img className="knivesImg" src='assets/Knives.png' alt="Procussions Logo"/>
+        </div>
+      </div>
     </div>
   )
 }
