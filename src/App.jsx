c8ad5782cdf16fe5b897262dffb8b68a53b3cca4
Seth <Medeirosseth@gamil.com>
2022-05-03 18:41:02 -0700

    update idea to have main current work photo

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import TopBar from './Components/TopBar/TopBar';
 import Live from './Components/Live/Live';
 import Lyrics from './Components/Lyrics/Lyric';
 import Pics from './Components/Pics/Pics';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
     <>
     <TopBar/>
     <div className="sections">
+      <Photo/>
       <Live />
       <Lyrics />
       <MusicVideos />
---
+++ src/Components/Photo/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./photo.css"
+export default function Photo() {
+  return (
+    <div className="photo">
+      <div className="photoImgContainer"> 
+        <img className="mainPhoto" src="assets/ofGodsAndGirls.jpg" alt="main" />
+      </div>
+    </div>
+  )
+}
